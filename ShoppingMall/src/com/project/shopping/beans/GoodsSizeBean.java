ef79982fdd39a20bae7ca12e68867494774bbe61
웅 <@DESKTOP-Q1PKB42>
2021-05-07 00:43:43 +0900

    goods_size bean mapper까지 만듬 나머지 만들고 detail페이지 완성하기...

---
+++ ShoppingMall/src/com/project/shopping/beans/GoodsSizeBean.java
@@ -0,0 +1,65 @@
+package com.project.shopping.beans;
+
+public class GoodsSizeBean {
+
+	private int goods_size_idx;
+	private int goods_idx;
+	private int goods_size_num;
+	private String goods_size_name;
+	private int goods_size_length;
+	private int goods_size_shoulder;
+	private int goods_size_chest;
+	private int goods_size_sleeve;
+	
+	public int getGoods_size_idx() {
+		return goods_size_idx;
+	}
+	public void setGoods_size_idx(int goods_size_idx) {
+		this.goods_size_idx = goods_size_idx;
+	}
+	public int getGoods_idx() {
+		return goods_idx;
+	}
+	public void setGoods_idx(int goods_idx) {
+		this.goods_idx = goods_idx;
+	}
+	public int getGoods_size_num() {
+		return goods_size_num;
+	}
+	public void setGoods_size_num(int goods_size_num) {
+		this.goods_size_num = goods_size_num;
+	}
+	public String getGoods_size_name() {
+		return goods_size_name;
+	}
+	public void setGoods_size_name(String goods_size_name) {
+		this.goods_size_name = goods_size_name;
+	}
+	public int getGoods_size_length() {
+		return goods_size_length;
+	}
+	public void setGoods_size_length(int goods_size_length) {
+		this.goods_size_length = goods_size_length;
+	}
+	public int getGoods_size_shoulder() {
+		return goods_size_shoulder;
+	}
+	public void setGoods_size_shoulder(int goods_size_shoulder) {
+		this.goods_size_shoulder = goods_size_shoulder;
+	}
+	public int getGoods_size_chest() {
+		return goods_size_chest;
+	}
+	public void setGoods_size_chest(int goods_size_chest) {
+		this.goods_size_chest = goods_size_chest;
+	}
+	public int getGoods_size_sleeve() {
+		return goods_size_sleeve;
+	}
+	public void setGoods_size_sleeve(int goods_size_sleeve) {
+		this.goods_size_sleeve = goods_size_sleeve;
+	}
+	
+	
+	
+}
